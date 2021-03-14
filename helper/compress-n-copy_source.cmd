637e2ea3282ff6b185e22c3d32e70fd8cfaebff6
digixx <uwe@gartmann.net>
2021-03-14 20:33:54 +0100

    added links for mpy-cross infos

--- helper/compress-n-copy_source.cmd
+++ helper/compress-n-copy_source.cmd
@@ -1,7 +1,7 @@
 REM Hepler Batchfile for converting .py files into compressed .mpy
 REM
-REM
-REM
+REM Find infos about mpy-cross here: https://learn.adafruit.com/creating-and-sharing-a-circuitpython-library?view=all
+REM Executables here: https://cdn-learn.adafruit.com/assets/assets/000/077/960/original/mpy-cross.zip?1562633743
 
 SET SourceFolder=D:\Dropbox\Development\Github\APRS-Buddy\circuitpython
 SET TempFolder=D:\Dropbox\Development\Projects\HAM\APRS_Buddy\Code\CircuitPython\copy_jobs\temp
